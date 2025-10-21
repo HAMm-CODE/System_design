487380766bec61949bf5458f5673fc9bda2c0b97
Hamm-code <hamuzahsentongo2@gmail.com>
2025-10-22 00:51:46 +0300

    working code

--- src/main/java/org/example/Api.java
+++ src/main/java/org/example/Api.java
@@ -11,7 +11,7 @@ public class Api {
     public User fetchUserById(int userId) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1" + userId))
+                    .uri(URI.create("https://jsonplaceholder.typicode.com/posts/" + userId))
                     .header("Accept", "application/json")
                     .build();
 
@@ -19,27 +19,7 @@ public class Api {
             return gson.fromJson(response.body(), User.class);
 
         } catch (Exception e) {
-            // Instead of throwing, just return null or a default object
             return null;
         }
     }
 }
-
-//public class Api {
-//    public User fetchUserById(int userId) throws Exception {
-//        // Make API call
-//        HttpClient client = HttpClient.newHttpClient();
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1" + userId))
-//                .build();
-//
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        String json = response.body();
-//
-//        // Parse JSON
-//        Gson gson = new Gson();
-//        User user = gson.fromJson(json, User.class);
-//
-//        return user;
-//    }
-//}
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -36,7 +36,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "userId=" + id +
+                "userId=" + id + '\n' +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", body='" + body + '\'' +
